adc5f822196ad9c3fcfdefff1a121ecc04918690
Simar rajput <simarjeet.rajput@teamlance.io>
2020-03-05 17:22:15 +0530

    added login fuctionality

--- ReactNativeBaseKit/src/containers/messages/index.js
+++ ReactNativeBaseKit/src/containers/messages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import Constants from '../../constants';
 
 const styles = StyleSheet.create({
@@ -7,8 +7,23 @@ const styles = StyleSheet.create({
     backgroundColor: Constants.Colors.DASHBOARD_BG_COLOR,
     flex: 1,
   },
+  itemContainer: {
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  textStyle: {
+    ...Constants.Fonts.extraLargeBold,
+    color: Constants.Colors.BLACK,
+    marginTop: (Constants.BaseStyle.DEVICE_HEIGHT / 100) * 2,
+  }
 });
 
-const Messages = () => <View style={styles.container} />;
+const Messages = () => 
+<View style={styles.container}>
+<View style={styles.itemContainer}>
+<Text style={styles.textStyle}> Message Screen </Text>
+</View>
+</View>;
 
 export default Messages;
